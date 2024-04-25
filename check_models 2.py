b0de9baa20c425c9ec17bf7b93cb0da58553e573
Daria Kashina <dvkashina@yandex.ru>
2024-04-25 19:10:29 +0300

    Update agent

--- check_models 2.py
+++
@@ -1,5 +0,0 @@
-from config import cred
-from langchain_community.chat_models import GigaChat
-
-chat = GigaChat(credentials=cred, verify_ssl_certs=False, scope='GIGACHAT_API_CORP')
-print(chat.get_models())
